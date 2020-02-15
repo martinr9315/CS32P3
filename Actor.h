78609c793fdd9b0c9298b87cdc30ca29bb8fadfe
Roxane Martin <36088627+martinr9315@users.noreply.github.com>
2020-02-15 15:07:41 -0800

    Add files via upload

---
+++ Actor.h
@@ -0,0 +1,12 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+class Actor: public GraphObject
+{
+    
+};
+
+#endif // ACTOR_H_
